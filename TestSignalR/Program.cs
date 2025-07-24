d3a7c2015a458102a290f2057fbef49d2ace7e2b
Mehedi53423 <mehedi53423@gmail.com>
2025-07-25 02:14:32 +0600

    Initial Commit

---
+++ TestSignalR/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var connection = new HubConnectionBuilder()
+            .WithUrl("http://localhost:5080/announcement")
+            .Build();
+
+        connection.On<object>("ReceiveAnnouncement", announcement =>
+        {
+            Console.WriteLine("Announcement received:");
+            Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(announcement, new System.Text.Json.JsonSerializerOptions { WriteIndented = true }));
+        });
+
+        await connection.StartAsync();
+        Console.WriteLine("Connected to SignalR hub.");
+
+        Console.ReadLine();
+    }
+}
